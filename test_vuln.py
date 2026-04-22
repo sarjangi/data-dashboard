759a3595111f0b2a6c55e8f7858388387c984559
VC_HO\\sararja <sara_arjangi@vancity.com>
2026-04-22 12:23:44 -0700

    cleanup: remove test SQL injection file [skip ci]

--- test_vuln.py
+++
@@ -1,7 +0,0 @@
-# test file with intentional vulnerability for pipeline testing
-import sqlite3
-
-def get_user(username):
-    conn = sqlite3.connect("app.db")
-    query = "SELECT * FROM users WHERE name = '" + username + "'"
-    return conn.execute(query).fetchall()
